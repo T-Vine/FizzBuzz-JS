15da801d6adde9939ce41145c1a32e49a0b6ab3d
T-Vine <132595704+T-Vine@users.noreply.github.com>
2023-06-02 18:33:45 +0100

    Buzz and FizzBuzz
    Added functionality for this.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let iterator = 0;
 let value1;
 let value2;
 let mainValue;
+let isFizzorBuzz;
 
 let errorMessage = document.querySelector("#errorMessage");
 
@@ -20,8 +21,8 @@ function myFunc(event) {
         iterator += 1; //incrementing iterator
 
         event.preventDefault();
-        let value1 = input.value;
-        let value2 = input.value;
+        value1 = input.value;
+        value2 = input.value;
         
         if (value1 >= iterator) {
             mainValue = value1;
@@ -34,9 +35,9 @@ function myFunc(event) {
             mainValue = parseInt(mainValue);
         }
         
-        console.log(typeof(mainValue)); //testing
+        /* console.log(typeof(mainValue)); //testing
         console.log(isNaN(mainValue)); //testing
-
+        */
         /* if (mainValue) {
             errorMessage.style.display = "block";
             console.log("Not integer") //testing
@@ -44,19 +45,43 @@ function myFunc(event) {
             if (iterator === mainValue) {
                 console.log("Equal");
             } 
-            if (iterator % 3 === 0) {
+
+            if (iterator % 3 === 0 &&  iterator % 5 === 0) {
+                if (String(mainValue).toUpperCase() == "FIZZBUZZ") {
+                    console.log("Fizz Buzz!");
+                } else {
+                    errorMessage.style.display = "block";
+                }
+            }
+
+            else if (iterator % 3 === 0) {
                 console.log("Modulo 3") //testing
                 if (String(mainValue).toUpperCase() == "FIZZ") {
                     console.log("Found fizz"); //testing if fizz  
                       
-                }
-                else {
+                } else {
                     errorMessage.style.display = "block";
                 }
-
-
+            }
+            else if (iterator % 5 === 0) {
+                console.log("Modulo 5");
+                if (String(mainValue).toUpperCase() == "BUZZ") {
+                    console.log("Found buzz.");
+                } else {
+                    errorMessage.style.display = "block";
+                }
             } 
-            if ((iterator !== mainValue) && (String(mainValue).toUpperCase() !== "FIZZ") && (String(mainValue).toUpperCase() !== "BUZZ")) {
+            if (iterator === 1 || iterator === 2) {
+                isFizzorBuzz = 1;
+                console.log(isFizzorBuzz+" is the fizz or buzz variable."); //testing
+            } else {
+                isFizzorBuzz = iterator % 3 + iterator % 5;
+                console.log(isFizzorBuzz+" is the fizz or buzz variable."); //testing
+            }
+            
+            strMain = String(mainValue).toUpperCase();
+
+            if ((iterator !== mainValue) && (strMain !== "FIZZ"  /*&& isFizzorBuzz === 0 */) && (strMain !== "BUZZ" /* && isFizzorBuzz === 0*/) && (strMain !== "FIZZBUZZ" /*&& isFizzorBuzz === 0*/)) {
                 console.log(mainValue, iterator)
                 console.log("Not equal.") //testing if equal
                 errorMessage.style.display = "block";
